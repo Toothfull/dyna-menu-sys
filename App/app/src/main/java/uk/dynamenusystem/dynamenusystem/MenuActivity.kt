0b013a58537024bf8b2c4f8270c95407c5ac7121
Toothfull <60007976+toothfull@users.noreply.github.com>
2023-02-24 15:51:21 +0000

    Created app draw for android app

---
+++ App/app/src/main/java/uk/dynamenusystem/dynamenusystem/MenuActivity.kt
@@ -0,0 +1,16 @@
+package uk.dynamenusystem.dynamenusystem
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MenuActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu)
+
+
+
+
+
+    }
+}
