e55d2131c449ddcd7fa8c36339b6ead2c6ba51ff
Toothfull <60007976+toothfull@users.noreply.github.com>
2023-02-24 17:42:51 +0000

    Added the ability to hide android UI

--- App/app/src/main/java/uk/dynamenusystem/dynamenusystem/MenuActivity.kt
+++ App/app/src/main/java/uk/dynamenusystem/dynamenusystem/MenuActivity.kt
@@ -2,15 +2,37 @@ package uk.dynamenusystem.dynamenusystem
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 
 class MenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
 
+        fun hideSystemUI() {
+            WindowCompat.setDecorFitsSystemWindows(window, false)
+            WindowInsetsControllerCompat(window, findViewById(R.id.menuConstraintLayout)).let { controller ->
+                controller.hide(WindowInsetsCompat.Type.systemBars())
+                controller.systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+            }
+        }
 
+        fun showSystemUI() {
+            WindowCompat.setDecorFitsSystemWindows(window, true)
+            WindowInsetsControllerCompat(window, findViewById(R.id.menuConstraintLayout)).show(WindowInsetsCompat.Type.systemBars())
+        }
 
+        hideSystemUI()
 
 
     }
 }
+
+
+
+
+
