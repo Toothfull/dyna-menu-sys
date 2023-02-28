76c05c2c0873939854a9b0e1c466578d7830561c
Toothfull <60007976+Toothfull@users.noreply.github.com>
2023-02-28 13:06:01 +0000

    add space

--- Browser/scripts/login.js
+++ Browser/scripts/login.js
@@ -16,7 +16,7 @@ $('#googleOAuth').click(function() {
 	// Get OAuth link from route
 	$.get('/oauthlink', function(url) {
 		// Redirect to Google OAuth
-		window.location.href = url;
+		window.location.href = url; 
 	});
 	
 });
