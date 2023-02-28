eb499a08c8a2f15bdc8f8fdc823da54fcb4d88cd
Toothfull <60007976+Toothfull@users.noreply.github.com>
2023-02-28 13:16:53 +0000

    run CI

--- Server/typescript/routes/oauthlink.ts
+++ Server/typescript/routes/oauthlink.ts
@@ -15,5 +15,5 @@ if(!clientID || !uri){
 app.get('/oauthlink', (request, response) => {
 	const state = randomString(16)
 	request.session.state = state
-	response.send(`https://accounts.google.com/o/oauth2/v2/auth?client_id=${clientID}&redirect_uri=http://${uri}/authorisedoauth&response_type=code&scope=https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile&access_type=online&state=${state}&prompt=consent&include_granted_scopes=true`)
-}) 
+	response.send(`https://accounts.google.com/o/oauth2/v2/auth?client_id=${clientID}&redirect_uri=${uri}/authorisedoauth&response_type=code&scope=https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile&access_type=online&state=${state}&prompt=consent&include_granted_scopes=true`)
+})  
